3d7d8d81d14f1b173d5bb4830ebb99c5670f8bcb
Kxmbrly Mae Alipin <93648323+kxmbrlydesu@users.noreply.github.com>
2025-01-01 19:01:39 +0800

    setting up pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
+import Login from './pages/Login/Login'
+import Chat from './pages/Chat/Chat'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/profile' element={<ProfileUpdate/>}/>
+      </Routes> 
+    </>
   )
 }
 
---
+++ src/pages/Chat/Chat.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Chat.css'
+
+const chat = () => {
+  return (
+    <div>chat</div>
+  )
+}
+
+export default chat
