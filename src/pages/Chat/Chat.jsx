9ba99ff1982fb88ea4573b0dea46dbce38ddd544
Kaemi <93648323+kxmashi@users.noreply.github.com>
2025-01-03 20:12:51 +0000

    Login/Signup frontend done

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Chat.css'
+import React from "react";
+import "./Chat.css";
 
 const chat = () => {
   return (
-    <div>
-      We are on the chat page.
+    <div className="chat">
+      <div className="chat-container"></div>
     </div>
-  )
-}
+  );
+};
 
-export default chat
+export default chat;
