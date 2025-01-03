8faf5d05cb08282b15444456862a821a22d78c13
Kaemi <93648323+kxmashi@users.noreply.github.com>
2025-01-03 11:33:18 +0000

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
-import Login from './pages/login/login'
-import Chat from './pages/chat/chat'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
+import Login from "./pages/Login/Login";
+import Chat from "./pages/Chat/Chat";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/chat' element={<Chat/>}/>
-        <Route path='/profile' element={<ProfileUpdate/>}/>
-      </Routes> 
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<ProfileUpdate />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,30 +1,48 @@
-import React, { useState } from 'react'
-import './Login.css'
-import assets from '../../assets/assets'
+import React, { useState } from "react";
+import "./Login.css";
+import assets from "../../assets/assets";
 
 const login = () => {
-
   const [currState, setCurrState] = useState("Sign Up");
 
   return (
-    <div className='login'>
-      <img src={assets.logo_big} alt="" className='logo'/>
+    <div className="login">
+      <img src={assets.logo_big} alt="" className="logo" />
       <form action="" className="login-form">
         <h2>Sign Up</h2>
-        <input type="text" placeholder='Username' className="form-input" autoFocus required/>
-        <input type="email" placeholder='Email Address' className="form-input" required/>
-        <input type="password" placeholder='Password' className="form-input" required/>
-        <button type='submit'>Sign Up</button>
+        <input
+          type="text"
+          placeholder="Username"
+          className="form-input"
+          autoFocus
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form-input"
+          required
+        />
+        <button type="submit">Sign Up</button>
         <div className="login-term">
-          <input type="checkbox" id="remember-me" name="remember-me"/>
+          <input type="checkbox" id="remember-me" name="remember-me" />
           <p>Agree to the terms of user & privacy policy</p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}> click here</span></p>
+          <p className="login-toggle">
+            Already have an account{" "}
+            <span onClick={() => setCurrState("Login")}> click here</span>
+          </p>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default login
+export default login;
