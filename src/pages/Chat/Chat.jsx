61b4abee40001903647236c9991525d8bd4bdc30
Kxmbrly Mae Alipin <93648323+kxmbrlydesu@users.noreply.github.com>
2025-01-05 22:41:32 +0800

    chatbox setup

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Chat.css";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar"
+import RightSidebar from "../../components/RightSidebar/RightSidebar"
+import Chatbox from "../../components/Chatbox/Chatbox"
+
 
 const Chat = () => {
   return (
     <div className="chat">
-      <div className="chat-container"></div>
+      <div className="chat-container">
+        <LeftSidebar />
+        <Chatbox />
+        <RightSidebar />
+      </div>
     </div>
   );
 };
