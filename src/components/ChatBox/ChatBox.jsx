3e1026d7bacc68397f38a2fe8d29072bb3fa315a
Kxmbrly Mae Alipin <93648323+kxmbrlydesu@users.noreply.github.com>
2025-01-05 22:55:15 +0800

    chatbox

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Chatbox.css";
+import assets from '../../assets/assets'
 
 const ChatBox = () => {
-  return <div></div>;
+  return (
+    <div className="chat-box">
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>Richard Sanford <img src={assets.green_dot} alt="" /></p>
+        <img src={assets.help_icon} className="help" alt="" />
+      </div>
+    </div>
+  )
 };
 
 export default ChatBox;
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import "./Chat.css";
 import LeftSidebar from "../../components/LeftSidebar/LeftSidebar"
 import RightSidebar from "../../components/RightSidebar/RightSidebar"
-import Chatbox from "../../components/Chatbox/Chatbox"
-
+import ChatBox from "../../components/Chatbox/Chatbox"
 
 const Chat = () => {
   return (
     <div className="chat">
       <div className="chat-container">
         <LeftSidebar />
-        <Chatbox />
+        <ChatBox />
         <RightSidebar />
       </div>
     </div>
